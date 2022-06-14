e28319b84df6a78b63f4b1c2f187fbeb801468d1
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2022-06-14 11:27:50 +1000

    line breaks!

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 let isVersionDisplay = false
 
 basic.forever(function () {
+
     if (!isVersionDisplay) {
+
         if (input.buttonIsPressed(Button.A)) {
             led.plotBrightness(0, 0, 255)
             radio.sendValue("a", 255)
@@ -9,6 +11,7 @@ basic.forever(function () {
             led.plotBrightness(0, 0, 0)
             radio.sendValue("a", 0)
         }
+
         if (input.buttonIsPressed(Button.B)) {
             led.plotBrightness(1, 0, 255)
             radio.sendValue("b", 255)
@@ -16,6 +19,7 @@ basic.forever(function () {
             led.plotBrightness(1, 0, 0)
             radio.sendValue("b", 0)
         }
+
         if (input.isGesture(Gesture.Shake)) {
             led.plotBrightness(2, 0, 255)
            radio.sendValue("s", 255)
@@ -23,6 +27,7 @@ basic.forever(function () {
             led.plotBrightness(2, 0, 0)
             radio.sendValue("s", 0)
         }
+
         if (input.logoIsPressed()) {
            led.plotBrightness(3, 0, 255)
             radio.sendValue("lp", 255)
@@ -30,6 +35,7 @@ basic.forever(function () {
             led.plotBrightness(3, 0, 0)
             radio.sendValue("lp", 0)
         }
+
         if (input.pinIsPressed(TouchPin.P0)) {
             led.plotBrightness(0, 4, 255)
             radio.sendValue("p0", 255)
@@ -37,6 +43,7 @@ basic.forever(function () {
             led.plotBrightness(0, 4, 0)
             radio.sendValue("p0", 0)
         }
+
         if (input.pinIsPressed(TouchPin.P1)) {
             led.plotBrightness(1, 4, 255)
             radio.sendValue("p1", 255)
@@ -44,6 +51,7 @@ basic.forever(function () {
             led.plotBrightness(1, 4, 0)
             radio.sendValue("p1", 0)
         }
+
         if (input.pinIsPressed(TouchPin.P2)) {
             led.plotBrightness(2, 4, 255)
             radio.sendValue("p2", 255)
@@ -51,31 +59,40 @@ basic.forever(function () {
             led.plotBrightness(2, 4, 0)
             radio.sendValue("p2", 0)
         }
-        // These last few ifs have no radio sending, this is because we don't have the space on the line to send the data over to another Micro:Bit
+
+        // These last few ifs have no radio sending, 
+        // this is because we don't have the space on the line to send the data over to another Micro:Bit
+
         if (input.isGesture(Gesture.LogoUp)) {
             led.plotBrightness(0, 2, 255)
         } else {
             led.plotBrightness(0, 2, 0)
         }
+
         if (input.isGesture(Gesture.LogoDown)) {
             led.plotBrightness(1, 2, 255)
         } else {
             led.plotBrightness(1, 2, 0)
         }
+
         if (input.isGesture(Gesture.TiltLeft)) {
             led.plotBrightness(2, 2, 255)
         } else {
             led.plotBrightness(2, 2, 0)
         }
+
         if (input.isGesture(Gesture.TiltRight)) {
             led.plotBrightness(3, 2, 255)
         } else {
             led.plotBrightness(3, 2, 0)
         }
+
         // No pixel setting on 4, 2 because the events I had planned didn't work, unfortunately.
+
         led.plotBrightness(4, 0, input.soundLevel())
         led.plotBrightness(3, 4, input.lightLevel())
         led.plotBrightness(4, 4, input.temperature() * 7.96875)
+
         radio.sendValue("sl", input.soundLevel())
         radio.sendValue("ll", input.lightLevel())
         radio.sendValue("temp", input.temperature() * 7.96875)
@@ -83,9 +100,11 @@ basic.forever(function () {
 })
 
 input.onButtonPressed(Button.AB, function() {
+
     isVersionDisplay = true
 
     basic.showString("1.0")
 
     isVersionDisplay = false
+    
 })
